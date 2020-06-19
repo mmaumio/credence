3a3e8fe935393fa477f72d975fda86f4df650d33
Zabid Ahmed <zabid.devscred@gmail.com>
2020-06-19 14:32:23 +0600

    fixed related post issue, query with tag.

--- template-parts/related-post.php
+++ template-parts/related-post.php
@@ -7,27 +7,29 @@
  */
 
 $cred_enable_related_post = get_theme_mod( 'cred_enable_related_post', 1 );
-$show_related_post = true;
+$cred_post_tags = wp_get_post_tags($post->ID);
 
-if( $cred_enable_related_post && is_single() && 'post' === get_post_type() && true === $show_related_post ) :
-    $related = get_posts( 
-        array( 
-            'category__in' => wp_get_post_categories( $post->ID ), 
-            'numberposts'  => 3, 
-            'post__not_in' => array( $post->ID ) 
-        ) 
+if( $cred_enable_related_post && is_single() && 'post' === get_post_type() && $cred_post_tags ) :
+    $first_tag = $cred_post_tags[0]->term_id;
+
+    $args=array(
+        'tag__in'          => array($first_tag),
+        'post__not_in'     => array($post->ID),
+        'posts_per_page'   => 3,
+        'ignore_sticky_posts ' => 1
     );
 
-    if( ! empty( $related ) ) : ?>
+    $my_query = new WP_Query( $args );
+    if( $my_query->have_posts() ) : ?>
         <div class="cred-post">
             <div class="cred-related-post-items row">
                 <div class="col-lg-12">
                     <h3 class="related-post-heading"><?php _e( 'Related Posts', 'credence' ); ?></h3>
                 </div>
                 <?php
-                foreach( $related as $cred_each_related_post ) :
-                    setup_postdata($cred_each_related_post); 
-                ?>
+                while( $my_query->have_posts() ) :
+                    $my_query->the_post();
+                    ?>
                     <div class="cred-single-related-post col-lg-4">
                         <?php if( has_post_thumbnail() ) : ?>
                             <div class="cred-related-post-thumb">
@@ -43,10 +45,12 @@ if( $cred_enable_related_post && is_single() && 'post' === get_post_type() && tr
                             <p><?php echo wp_trim_words( wp_kses_post( get_the_excerpt() ), apply_filters( 'cred_related_post_excerpt_number', 15 ) ); ?></p>
                         </div>
                     </div>
-                <?php endforeach; ?>
+                <?php endwhile; ?>
             </div>
         </div>
     <?php
     endif;
     wp_reset_postdata();
 endif;
+
+
