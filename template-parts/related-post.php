8bec994d7cf3f9a568a06efe5a643482043d2a51
Zabid Ahmed <zabid.devscred@gmail.com>
2020-06-19 18:42:29 +0600

    fixed variable issue at relative post.

--- template-parts/related-post.php
+++ template-parts/related-post.php
@@ -19,17 +19,17 @@ if( $cred_enable_related_post && is_single() && 'post' === get_post_type() && $c
         'ignore_sticky_posts ' => 1
     );
 
-    $my_query = new WP_Query( $args );
-    
-    if( $my_query->have_posts() ) : ?>
+    $query = new WP_Query( $args );
+
+    if( $query->have_posts() ) : ?>
         <div class="cred-post">
             <div class="cred-related-post-items row">
                 <div class="col-lg-12">
                     <h3 class="related-post-heading"><?php _e( 'Related Posts', 'credence' ); ?></h3>
                 </div>
                 <?php
-                while( $my_query->have_posts() ) :
-                    $my_query->the_post();
+                while( $query->have_posts() ) :
+                    $query->the_post();
                     ?>
                     <div class="cred-single-related-post col-lg-4">
                         <?php if( has_post_thumbnail() ) : ?>
